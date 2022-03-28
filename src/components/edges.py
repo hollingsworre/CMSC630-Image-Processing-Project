9dbe3bf8b46ccaccf0ee3f727c4ec49c48e80c5f
hollingsworre <78879630+hollingsworre@users.noreply.github.com>
2022-03-28 15:00:45 -0400

    Update edges.py

--- src/components/edges.py
+++ src/components/edges.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numba import jit
+import math
 
 
 class Edges:
@@ -97,10 +98,28 @@ class Edges:
     @staticmethod
     @jit(nopython=True,cache=True)
     def edge_dilation(image_edges, num_layers=1):
-        # structuring element                                        
-        structuring_element = np.array([[True,False,True],
-                                        [False,True,False],
-                                        [True,False,True]])
+        """
+        Perform dilation of an edge (black and white, eg 0 or 255) based image
+        """
+
+        # Quasi-Disk shaped structuring elements                                     
+        _structuring_element = np.array([[False,True,True,True,False],
+                                        [True,True,True,True,True],
+                                        [True,True,True,True,True],
+                                        [True,True,True,True,True],
+                                        [False,True,True,True,False]])
+
+        structuring_element = np.array([[True,True,True],
+                                        [True,True,True],
+                                        [True,True,True]])
+
+        __structuring_element = np.array([[False,True,False],
+                                        [True,True,True],
+                                        [False,True,False]])
+
+        structure_height, structure_width = structuring_element.shape
+        floor_structure_height = math.floor(structure_height/2)
+        floor_structure_width = math.floor(structure_width/2)
 
         # get size of image
         height, width = image_edges.shape
@@ -112,15 +131,17 @@ class Edges:
         for _ in range(num_layers):
             # go through every pixel of image
             # replace black pixel (intensity == 0) with the structuring element
-            for row in range(1,height-1):
-                for column in range(1,width-1):
+            for row in range(floor_structure_height,height-floor_structure_height):
+                for column in range(floor_structure_width,width-floor_structure_width):
                     # if over an edge pixel apply the structuring element to it (intensity 0 == black)
                     if image_edges_copy[row][column] == 0:
                         # apply structuring element for edge dilation
                         # numpy.where() iterates over the structuring element bool array
                         # and for every True it yields corresponding element from the first list (0 == for a black edge pixel)
                         # and for every False it yields corresponding element from the second list
-                        edge_dilation_temp[row-1:row+2,column-1:column+2] = np.where(structuring_element, np.zeros((3,3)), image_edges_copy[row-1:row+2,column-1:column+2])
+                        edge_dilation_temp[row-floor_structure_height:row+floor_structure_height+1,column-floor_structure_width:column+floor_structure_width+1] = np.where(structuring_element,
+                                                                                                                                                                            np.zeros((structure_height,structure_width)),
+                                                                                                                                                                            image_edges_copy[row-floor_structure_height:row+floor_structure_height+1,column-floor_structure_width:column+floor_structure_width+1])
                         
             image_edges_copy = np.copy(edge_dilation_temp) # copy over in preparation for adding another layer
 
